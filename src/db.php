70c12db92d271bf7fd971d558bfcb4857aeee6f1
Agryzel <zaurelien2000@gmail.com>
2024-01-19 08:57:24 +0100

    modify db.php

--- src/db.php
+++ src/db.php
@@ -4,11 +4,11 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
 try {
-    $dsn = 'mysql:host=localhost; dbname=dressUpDay; port=8888';
+    $dsn = 'mysql:host=localhost;port=8888;dbname=dressUpDay';
     $db = new PDO($dsn, 'root', 'root');
     $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-}
-catch (Exception $e) {
-    echo $e->getMessage();
+} catch (PDOException $e) {
+    echo 'Connection failed: ' . $e->getMessage();
     die('Impossible de se connecter à la DB.');
 }
+
