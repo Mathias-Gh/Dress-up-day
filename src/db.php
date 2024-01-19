63d3ec035c51bce54e2d74759f001f8019cf31bc
Mathias.G <77049177+Mathias-Gh@users.noreply.github.com>
2024-01-19 15:24:22 +0100

    db

--- src/db.php
+++ src/db.php
@@ -4,7 +4,7 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
 try {
-     $dsn = 'mysql:host=localhost;port=8888;dbname=dressUpDay';
+    $dsn = 'mysql:host=127.0.0.1;dbname=dressUpDay;port=8889';
     $db = new PDO($dsn, 'root', 'root');
     $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 } catch (PDOException $e) {
