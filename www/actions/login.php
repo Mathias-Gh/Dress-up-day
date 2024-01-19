0e34ac276bf8af1dd7c5787267417dd09a31760d
Mathias.G <77049177+Mathias-Gh@users.noreply.github.com>
2024-01-19 10:08:39 +0100

    login marche

--- www/actions/login.php
+++ www/actions/login.php
@@ -2,29 +2,32 @@
 
 require_once __DIR__ . '/../../src/init.php';
 
-    if (!isset($_POST['loginEmail'])) {
-        echo "Pas d'email";
-        header('Location: /?p=register');
+    if (empty($_POST['loginEmail'])) {
+        $_SESSION['error_message'] = "Pas d'email";
+        header('Location: /?p=login');
         die();
     }
 
-    if (!isset($_POST['loginPassword'])) {
-        echo "Pas de password";
-        header('Location: /?p=register');
+    if (empty($_POST['loginPassword'])) {
+        $_SESSION['error_message'] = "Pas de mot de passe";
+        header('Location: /?p=login');
         die();
     }
 
     if (!filter_var($_POST['loginEmail'], FILTER_VALIDATE_EMAIL)) {
         echo "Email incorrect";
-        header('Location: /?p=register');
+        header('Location: /?p=login');
         die();
     }
 
     // to be verified
-    $user = $dbMan->getBy_advanced('email',  $_POST['email'], 'User');
+    $user = $dbMan->getBy_advanced('email',  $_POST['loginEmail'], 'User');
+
     if ($user !== false) {
         $validPassword = $user->verifyPassword($_POST['loginPassword']);
+
         if ($validPassword) {
+           
             // all good
             $_SESSION['id'] = $user->id;
             header('Location: /?p=home');
@@ -34,4 +37,4 @@ require_once __DIR__ . '/../../src/init.php';
 
 header('Location: /?p=home');
 // not good
-// set_errors('Bad login', 'login');
+set_errors('Bad login', 'login');
